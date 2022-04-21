947ee12d4cb9ec41559a92f1e35edbb1fcc19024
Abeer <abeer.technology@gmail.com>
2022-04-22 04:29:15 +0600

    product routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 // internal
 import checkAsyncError from "../middlewares/checkAsyncError.js";
 import Product from "../models/productModel.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 // Get Products
 export const getProducts = checkAsyncError(async (req, res, next) => {
@@ -14,8 +15,11 @@ export const getProducts = checkAsyncError(async (req, res, next) => {
 
 // Get Product Details
 export const productDetails = checkAsyncError(async (req, res, next) => {
-  const productId = req.params.id;
-  const product = await Product.findById(productId);
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
   res.status(200).json({
     success: true,
@@ -34,5 +38,40 @@ export const newProduct = checkAsyncError(async (req, res, next) => {
   res.status(201).json({
     success: true,
     product,
+  });
+});
+
+// Update A Product
+export const updateProduct = checkAsyncError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+// Delete A Product
+export const deleteProduct = checkAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  await product.remove();
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
   });
 });
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,15 +1,21 @@
 import express from "express";
 
 import {
+  deleteProduct,
   getProducts,
   newProduct,
   productDetails,
+  updateProduct,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/products").get(getProducts);
 router.route("/new-product").post(newProduct);
-router.route("/products/:id").get(productDetails);
+router
+  .route("/products/:id")
+  .get(productDetails)
+  .put(updateProduct)
+  .delete(deleteProduct);
 
 export default router;
