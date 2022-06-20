c7e85d4dd2f1d8209b6ed4cf6ea5a92989f2fcbf
Abeer <abeer.technology@gmail.com>
2022-06-20 18:48:28 +0600

    new way to run frontend and backend together

--- server/server.js
+++ server/server.js
@@ -8,30 +8,34 @@ import routes from "./routes/index.js";
 import errorMiddleware from "./middlewares/error.js";
 
 const app = express();
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 //Config
-dotenv.config({ path: "server/config/config.env" });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: "server/config/config.env" });
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../client/build")));
+
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Connect Database
 connectDB();
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
 
 // Routes
 routes(app);
 
 // Middleware for errors
 app.use(errorMiddleware);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join("client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on http://localhost:${process.env.PORT}`)
